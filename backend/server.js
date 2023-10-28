2b6a3057ce6ca0e6d81f3d2db4a7ab11d2ac3813
Vignesh <vigneshkumarav1998@gmail.com>
2023-10-28 14:31:35 +0530

    cors url added

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ const cors = require('cors')
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
 app.use(cors({
-  origin: ["http://localhost:3000/"]
+  origin: ["http://localhost:3000/","https://mern-task-app-api-vignesh.onrender.com"]
 }))
 app.use("/api/tasks",taskRoute);
 
